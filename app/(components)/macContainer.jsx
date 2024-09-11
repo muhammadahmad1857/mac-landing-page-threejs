e7ea16a4bcfc9d718b63b2466542e095d2cfcd45
Ahmad Jawad <ahmedjawad1857@gmail.com>
2024-09-11 20:43:47 +0500

    AddNewModel

--- app/(components)/macContainer.jsx
+++ app/(components)/macContainer.jsx
@@ -1,21 +1,45 @@
-import { useGLTF, useScroll, useTexture } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useLoader, useFrame } from "@react-three/fiber";
 import React, { useState, useEffect } from "react";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { useTexture } from "@react-three/drei";
+import { useScroll } from "@react-three/drei";
 
 const MacContainer = () => {
-  let model = useGLTF("/mac.glb");
-  let tex = useTexture("/red.jpg");
+  // Set up DRACOLoader
+  const model = useLoader(GLTFLoader, "/mac.glb", (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath(
+      "https://www.gstatic.com/draco/versioned/decoders/1.5.5/"
+    );
+    dracoLoader.setDecoderConfig({ type: "js" });
+    loader.setDRACOLoader(dracoLoader);
+  });
+
+  const tex = useTexture("/red.jpg");
   let meshes = {};
   model.scene.traverse((e) => {
     meshes[e.name] = e;
+    // console.log(e.name);
   });
 
-  meshes.screen.rotation.x = THREE.MathUtils.degToRad(-180);
-  meshes.matte.material.map = tex;
-  meshes.matte.material.emissiveIntensity = 0;
-  meshes.matte.material.metalness = 0;
-  meshes.matte.material.roughness = 1;
+  // Ensure that the mesh names exist before applying transformations
+  if (meshes.screen) {
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(-180);
+    console.log("Appplying screen");
+
+  }
+
+  if (meshes.matte) {  
+      console.log("Appplying mashes");
+
+    meshes.matte.material.map = tex;
+    meshes.matte.material.emissiveIntensity = 0;
+    meshes.matte.material.metalness = 0;
+    meshes.matte.material.roughness = 1;
+  }
+
   let data = useScroll();
 
   const [scale, setScale] = useState([1, 1, 1]);
@@ -40,7 +64,11 @@ const MacContainer = () => {
   }, []);
 
   useFrame((state, delta) => {
-    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - data.offset * 90);
+    if (meshes.screen) {
+      meshes.screen.rotation.x = THREE.MathUtils.degToRad(
+        180 - data.offset * 90
+      );
+    }
   });
 
   return (
